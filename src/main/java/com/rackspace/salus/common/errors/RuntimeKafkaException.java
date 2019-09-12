996c3eba0ed53859927687f66379ccfa423058b9
Adam <harageth@gmail.com>
2019-09-12 15:27:27 -0700

    added javadoc

--- src/main/java/com/rackspace/salus/common/errors/RuntimeKafkaException.java
+++ src/main/java/com/rackspace/salus/common/errors/RuntimeKafkaException.java
@@ -1,5 +1,9 @@
 package com.rackspace.salus.common.errors;
 
+/**
+ * We are creating this Runtime exception for Kafka so that we can track it and give a relevant message
+ * to customers when Kafka is down
+ */
 public class RuntimeKafkaException extends RuntimeException {
 
   public RuntimeKafkaException(Exception e) {
